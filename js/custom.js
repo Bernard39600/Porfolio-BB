2c58f76f1af3eb89b08b845a239a40c8a2cc560f
BBernard <b.brocard@codeur.online>
2022-08-02 11:33:18 +0200

    suppr fichier css

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 
-//
 $(document).ready(function() {
 			$('#fullpage').fullpage({
 				'verticalCentered': false,
@@ -17,19 +16,3 @@ $(function()
     new WOW().init();
     $(".rotate").textrotator();
 })
-
-
-// aff modal
-document.getElementById("voircv").addEventListener('click', function () {
-
-	console.log("passage modal 1")
-	document.getElementById('modal').classList.toggle("none")
-
-});
-//cache modal
-document.getElementById("modal").addEventListener('click', function () {
-
-	console.log("passage modal 2")
-	document.getElementById('modal').classList.toggle("none")
-
-});
